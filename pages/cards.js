2ed2702581db0d15f37c728b02d77de79081dd08
ELDADBRHANO <ebrhano@gmail.com>
2022-06-30 20:16:45 +0300

    made_it_byslo_commit

--- pages/cards.js
+++ pages/cards.js
@@ -1,20 +1,49 @@
-async function get(){
-  try {
-  return await fetch("https://my-json-server.typicode.com/Jeck99/fake-server/devices")
+let url = "https://my-json-server.typicode.com/Jeck99/fake-server/devices"
+async function get(html = " "){
+  let devices = await fetch(url)
   .then(res=>res.json())
-  } 
-  catch (err) {
-    catchError(err)
+  for (const key in devices) {
+    html+= cardTemplate(devices[key])
   }
+  document.getElementById("container_cards").innerHTML = html
+}
+get()
+
+function cardTemplate(device) {
+  let {id,brand,price,color,pic,isAvailable,ram,createdAt} = device
+  return  `<div id="card">
+  <div> brand : ${brand}  </div>
+  <div> price : ${price}</div>
+  <div> id : ${id}</div>
+  <div> color : ${color}</div>
+  <div> pic : ${pic}</div>
+  <div> Available : ${isAvailable}</div>
+  <div> ram : ${ram}</div>
+  <div> createdAt : ${createdAt}</div>
+  </div>
+  `
+  // return card.innerHTML += 
 }
 
-function getSome() {
-  get()
-  .then(res=>res.console.log(res))
-  catchSomething.innerHTML= res
-}
+
+// function catchError(error) {
+//   console.log(error);
+// }
 
 
-function catchError(error) {
-  console.log(error);
-}
+// return `
+// <div class="card">
+  // <div> brand : ${brand}</div>
+  // <div> price : ${price}</div>
+  // <div> id : ${id}</div>
+  // <div> color : ${color}</div>
+  // <div> pic : ${pic}</div>
+  // <div> Available : ${isAvailable}</div>
+  // <div> ram : ${ram}</div>
+  // <div> createdAt : ${createdAt}</div>
+//   </div>
+// `
+
+
+
+
