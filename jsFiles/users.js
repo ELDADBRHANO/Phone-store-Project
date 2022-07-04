8f95b56a376ec7fdc770f156e762b367cd02e1d8
ELDADBRHANO <ebrhano@gmail.com>
2022-07-04 09:28:55 +0300

    commit

--- jsFiles/users.js
+++ jsFiles/users.js
@@ -29,27 +29,33 @@ function printUser(user) {
  `;
 }
 
- async function registerUser(event) {
-  event.preventDefult()
-  let obj = getFormInfo(event)
-  let user = await fetch(API,{
-    method:'POST',
-    headers:{
-      'Content-Type':'application/json'
-    },
-    body:JSON.stringify(obj)
-  }).then(res=> res.json())
-  main.insertAdjacentHTML("beforeens",printUser(user))
- }
+//  async function registerUser(event) {
+//   event.preventDefult()
+//   let obj = getFormInfo(event)
+//   let user = await fetch(API,{
+//     method:'POST',
+//     headers:{
+//       'Content-Type':'application/json'
+//     },
+//     body:JSON.stringify(obj)
+//   }).then(res=> res.json())
+//   main.insertAdjacentHTML("beforeens",printUser(user))
+//  }
 
- function getFormInfo(event,obj={name:{}}) {
-  let elments = [...document.getElementsByTagName("input")]
-  elments.forEach(element =>{
-    if(element.name =='first'||element.name=='last'){
-      obj.name[element.name] = element.value
-    }else{
-      obj[element.name]= element.value
-    }
-  })
-  return obj
- }
+//  function getFormInfo(event,obj={name:{}}) {
+//   let elments = [...document.getElementsByTagName("input")]
+//   elments.forEach(element =>{
+//     if(element.name =='first'||element.name=='last'){
+//       obj.name[element.name] = element.value
+//     }else{
+//       obj[element.name]= element.value
+//     }
+//   })
+//   return obj
+//  }
+
+
+
+
+
+ 
